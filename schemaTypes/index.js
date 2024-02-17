9d1cbc643162a789f97011b0889568b7aef6a459
dunpaul <mwasmaish86@gmail.com>
2024-02-17 12:17:38 +0300

    dynamic gallery page

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,4 @@
-export const schemaTypes = []
+import student_gallery from "./student_gallery";
+import teachers_gallery from "./teachers_gallery";
+
+export const schemaTypes = [student_gallery, teachers_gallery]
